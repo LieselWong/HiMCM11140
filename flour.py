c26bbb6b4356e67e39a51d8230330ddcebc45e50
LieselWong <awesomesaucelw@gmail.com>
2020-11-12 18:29:13 -0500

    add AHP/mozzarella

--- flour.py
+++ flour.py
@@ -1,109 +1,30 @@
 import os
 import csv
-
-
-
 import dough
-
-
-
 import random
-
-
-
 import sauce
-
-
-
 #os.chdir("/Users/christinelee/Documents/Github/HiMCM11140")
-
-
-
 with open("rollingpin.csv") as csvfile:
+	reader = csv.reader(csvfile)
+	next(reader)
+	next(reader)
+	jobs = []
+	for row in reader:
+    		temp = []
+    		for char in "RIASEC":
+        			temp.append(char in row[2])
+    		jobs.append([row[0], float(row[1]), temp, float(row[5]), random.randint(1, 5)])
+		people = sauce.randPeople(10)
+		for person in people:
+    			applicantvalues, weights = person
+    			jobScores = []
+    			for job in jobs:
+        				jobvalues = dough.jobvaluesss(job[1:], applicantvalues)
+        				jobScores.append((dough.satisfaction(weights, jobvalues), job))
+    			jobScores.sort(key = lambda x: x[0])
+    			print("Top five jobs for this random person:")
+    			print(person)
+    			for job in jobScores[::-1][:5]:
+        				print(job)
+    			print("\n")
 
-
-
-reader = csv.reader(csvfile)
-
-
-
-next(reader)
-
-
-
-next(reader)
-
-
-
-jobs = []
-
-
-
-for row in reader:
-
-
-
-temp = []
-
-
-
-for char in "RIASEC":
-
-
-
-temp.append(char in row[2])
-
-
-
-jobs.append([row[0], float(row[1]), temp, float(row[5]), random.randint(1, 5)])
-
-
-
-people = sauce.randPeople(10)
-
-
-
-for person in people:
-
-
-
-applicantvalues, weights = person
-
-
-
-jobScores = []
-
-
-
-for job in jobs:
-
-
-
-jobvalues = dough.jobvaluesss(job[1:], applicantvalues)
-
-
-
-jobScores.append((dough.satisfaction(weights, jobvalues), job))
-
-
-
-jobScores.sort(key = lambda x: x[0])
-
-
-
-print("Top five jobs for this random person:")
-
-
-
-print(person)
-
-
-
-for job in jobScores[::-1][:5]:
-
-
-
-print(job)
-
-
-print("\n")
--- mozzarella.py
+++ mozzarella.py
@@ -1,25 +1,69 @@
 import random
-chart = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0]]
-sum = [0, 0, 0, 0, 0]
-for i in range(5):
-    for z in range(5):
-        if i == z:
-            chart[i][z] = 1
-        if chart[i][z] != 0:
-            continue
-        x = random.randint(1, 9)
-        chart[i][z] = x
-        chart[z][i] = 1/x
-print(chart)
+def AHP(): 
+    chart = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+    temp = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+    sum = [0, 0, 0, 0, 0, 0]
+    weights = [0, 0, 0, 0, 0, 0]
+    weightedsum = [0, 0, 0, 0, 0, 0]
+    lammax = 0
+    consistency = 0
+    for i in range(6):
+        for z in range(6):
+            if i == z:
+                chart[i][z] = 1
+            if chart[i][z] != 0:
+                continue
+            x = random.randint(1, 9)
+            chart[i][z] = x
+            chart[z][i] = 1/x
+    temp = chart
 
-for i in range(5):
-    for z in range(5):
-        sum[i] = sum[i] + chart[z][i]
-print(sum)
+    #add sum of each column 
+    for i in range(6):
+        for z in range(6):
+            sum[i] = sum[i] + chart[z][i]
+    
+    #divide each variable in column by sum of each column
+    for i in range(6):
+        for z in range(6):
+            chart[z][i] = chart[z][i]/sum[i]
+            
+    #gives criteria weights
+    for i in range(6):
+        for z in range(6):
+            weights[i] = chart[i][z] + weights[i]
+        weights[i] = weights[i]/6
+    #print(weights)
+    
+    #verifyingAHP
+    for i in range(6):
+        for z in range(6):
+            temp[z][i] = weights[i] * temp[z][i]
+    #findingweightesum
+    for i in range(6):
+        for z in range(6):
+            weightedsum[i] = weightedsum[i] + temp[i][z]
+    #dividing Criteria weights by weighted sums
+    for i in range(6):
+        weightedsum[i] = weights[i] / weightedsum[i]
+    #Adding weightedsum/criteria weights to get lambda max
+    for i in range(6):
+        lammax = weightedsum[i] + lammax
+        lammax = lammax / 6 #essentially taking average
 
-for i in range(5):
-    for z in range(5):
-        chart[z][i] = chart[z][i]/sum[i]
+    #consistency index is (lambda max - number of variables)/(num of variables -1)
+    consistency = (lammax - 6) / (6 - 1)
+    consistency = consistency / 1.24
+    #1.24 is dependent on random index aka 6 variables
 
-print(chart)
+    if consistency < 0.1:
+        print("bueno")
+    else:
+        print("no")
+
+    return weights 
+
+print(AHP())
+
